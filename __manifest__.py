46179131c0c90c81d71bafff0c1e26abe44bc891
Khushi Trivedi <khushictrivedi16@gmail.com>
2025-02-14 12:56:23 +0530

    Updated manifest, controllers, and email templates

--- __manifest__.py
+++ __manifest__.py
@@ -8,12 +8,13 @@
     This module works for both 'Enterprise' and 'Community'.
     """,
     "license": "LGPL-3",
-    "depends": ["base","website"],
+    "depends": ["base","website","mail"],
     "data": [
         "security/ir.model.access.csv",
         "views/loan_menu.xml",
         "views/inquiry_views.xml",
         "views/inquiry_templates.xml",
+        "data/templates_email.xml",
     ],
     "installable": True,
     "application": True,
--- controllers/main.py
+++ controllers/main.py
@@ -10,11 +10,15 @@ class LoanInquiryController(http.Controller):
     @http.route('/loan/inquiry/submit', type='http', auth="public", methods=['POST'], website=True, csrf=True)
     def submit_loan_inquiry(self, **post):
         if post:
-            request.env['loan.inquiry'].sudo().create({
+            inquiry = request.env['loan.inquiry'].sudo().create({
                 'client_name': post.get('client_name'),
                 'email': post.get('email'),
                 'phone_number': post.get('phone_number'),
                 'gender': post.get('gender'),
                 'city': post.get('city'),
             })
+
+            template = request.env.ref('loan_management.email_template_loan_inquiry')
+            if template:
+                template.sudo().send_mail(inquiry.id, force_send=True)
         return request.render("loan_management.inquiry_success", {})
