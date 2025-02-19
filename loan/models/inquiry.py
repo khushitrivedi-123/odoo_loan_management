63b55e889e17404d7ee6304cd8cdc0dd96c6e8f1
dhruvaparsaniya <156284908+dhruvaparsaniya@users.noreply.github.com>
2025-02-19 14:03:27 +0530

    Update with status fiedld and user creation

--- loan/models/inquiry.py
+++ loan/models/inquiry.py
@@ -7,7 +7,11 @@ class LoanInquiry(models.Model):
     _name = "loan.inquiry"
     _description = "Loan Inquiry"
     _rec_name = "client_name"
+    _inherits = {"res.users": "u_id"}
 
+    u_id = fields.Many2one(
+        "res.users", required=True, ondelete="restrict", auto_join=True
+    )
     client_name = fields.Char(string="Client Name", required=True)
     email = fields.Char(string="Email", required=True)
     phone_number = fields.Char(string="Phone Number", required=True)
@@ -15,6 +19,11 @@ class LoanInquiry(models.Model):
     city = fields.Selection([("ahmedabad","Ahmedabad"),("mumbai", "Mumbai"),("delhi", "Delhi")], string="City", required=True)
     state = fields.Selection([("gujarat","Gujarat"),("maharashtra","Maharashtra"),("delhi", "Delhi")], string="State", required=True)
     pincode = fields.Char(string="Pincode", required=True)
+    status = fields.Selection(
+        [("draft", "Draft"), ("approve", "approve"), ("reject", "Reject")],
+        default="draft",
+        tracking=True
+    )
 
     @api.constrains("email")
     def _check_email_format(self):
@@ -39,4 +48,22 @@ class LoanInquiry(models.Model):
                 pincode_regex = r"^\d{6}$"  # Ensures exactly 6 digits
                 if not re.match(pincode_regex, client.pincode):
                     raise ValidationError("Pincode must contain exactly 6 digits.")
+    @api.model
+    def create(self, vals):
+        vals['name'] = vals.get("client_name")
+        vals['login'] = vals.get("email")
+        user = super(LoanInquiry, self).create(vals)
+        return user
+
+    def action_approve(self):
+        for record in self:
+            record.status = 'approve'
+
+    def action_reject(self):
+        for record in self:
+            record.status = 'reject'
+
+    def action_draft(self):
+        for record in self:
+            record.status = 'draft'
 
