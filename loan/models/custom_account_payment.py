b2e586a4f5dccc435d53df3b811d3d8e2c7dff3d
dhruvaparsaniya <156284908+dhruvaparsaniya@users.noreply.github.com>
2025-02-20 17:40:23 +0530

    Add sequence in ref

--- loan/models/custom_account_payment.py
+++ loan/models/custom_account_payment.py
@@ -9,32 +9,31 @@ class CustomAccountPayment(models.Model):
 
     @api.model
     def create(self, vals):
+        loan_installment_id = self.env.context.get('default_loan_installment_id')
+
+        if not loan_installment_id:
+            print("loan_installment_id is missing in vals and context!")
+
+        else:
+            loan_installment = self.env['loan.installment'].browse(loan_installment_id)
+            client_id = loan_installment.loan_id.loan_id
+            sequence = self.env['ir.sequence'].next_by_code('account.payment.ref') or 'new'
+            vals['ref'] = f'{client_id}{sequence}'
+
         payment = super(CustomAccountPayment, self).create(vals)
-        amount = payment.amount
-        installment_amount = payment.installment_amount
-        if installment_amount != amount:
-            print("--------------------",payment.installment_amount,"----------------------",amount)
+        if payment.installment_amount > 0.00:
+            amount = payment.amount
+            installment_amount = payment.installment_amount
+            if installment_amount != amount:
+                raise UserError(
+                    f"Invalid amount!\n"
+                    f"Exact remaining amount for this installment is {installment_amount}.\n"
+                    "To make a custom payment, please click on the Custom Payments button on the loan form."
+                )
 
-            raise UserError(
-                f"Invalid amount!\n"
-                f"Exact remaining amount for this installment is {installment_amount}.\n"
-                "To make a custom payment, please click on the Custom Payments button on the loan form."
-            )
-
-        if payment.loan_installment_id:
-            payment.loan_installment_id.payment_ref = payment.ref
-            payment.loan_installment_id.payment_date = payment.date
+            if payment.loan_installment_id:
+                payment.loan_installment_id.payment_ref = payment.ref
+                payment.loan_installment_id.amount_paid = payment.amount
+                payment.loan_installment_id.payment_date = payment.date
+                payment.loan_installment_id.status = 'paid'
         return payment
-
-
-
-        #loan_id = vals.get(" self.loan_id.inquiry_id.partner_id.id,")
-
-        # loan_prefix = f"{loan_id}/INSTLMT"
-        # if "ref" not in vals or not vals["ref"]:  # Set ref only if it's empty
-        #     sequence_number = self.env["ir.sequence"].next_by_code("memo.sequence.data") or "New"
-        #     vals["ref"] = f"{loan_prefix}{sequence_number}"
-        # return super(CustomAccountPayment, self).create(vals)
-
-
-
